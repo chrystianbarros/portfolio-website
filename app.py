5ec2f9d89c20672fa3661f593dd6f83925db175e
Chrystian Barros <chrystian.barros@hotmail.com>
2024-02-15 12:48:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, world!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=1443, debug=True)
+app.run(host='0.0.0.0', port=1443, debug=True)
