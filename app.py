cd7a9a8c4a3d0ce2fe7e496ca8517e95d1a0bbd7
Chrystian Barros <chrystian.barros@hotmail.com>
2024-02-16 12:40:28 -0300

    Added dynamic python data and API

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+SKILLS = [
+    {
+        'skill': 'Python',
+        'type': 'Programming Languages',
+        'main uses': 'Application development and automation'
+    },
+    {
+        'skill': 'Powershell',
+        'type': 'Programming/Scripting Languages',
+        'main uses': 'Automation and scripting'
+    },
+    {
+        'skill': 'Microsoft Azure',
+        'type': 'Public Cloud Platform',
+        'main uses': 'Architecting and infrastructure deployment and management'
+    },
+    {
+        'skill': 'Oracle Cloud Infrastructure',
+        'type': 'Public Cloud Platform',
+        'main uses': 'Architecting and infrastructure deployment and management'
+    },
+    {
+        'skill': 'Linux',
+        'type': 'Operational System',
+        'main uses': 'Infrastructure and support operations'
+    },
+    {
+        'skill': 'SQL',
+        'type': 'Programming Language',
+        'main uses': 'Database operations and scripting'
+    }
+]
+
 @app.route("/")
 def portfolio_website():
-    return render_template('home.html')
+    return render_template('home.html', skills=SKILLS)
+
+@app.route("/api/mainSkills")
+def main_skills():
+    return jsonify(SKILLS)
 
 app.run(host='0.0.0.0', port=1443, debug=True)
