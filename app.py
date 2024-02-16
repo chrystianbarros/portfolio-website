fb2577a48c25d264a90e830cd64068d6f4f3095f
Chrystian Barros <chrystian.barros@hotmail.com>
2024-02-16 11:41:12 -0300

    Added bootstrap and design improvements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def portfolio_website():
     return render_template('home.html')
 
 app.run(host='0.0.0.0', port=1443, debug=True)
